54e49b6a7c98b4b4584a18cd45c4d604860ef6a3
mathistite <tucayjerry3@gmail.com>
2019-03-18 18:31:19 -0700

    put graphs in for mar

--- R/mar.R
+++ R/mar.R
@@ -10,10 +10,35 @@ mar <- function(data, p, q, column = NULL){
       m.idx <- (rbinom(n=NROW(data), size=1, prob=q*(U > p))==1)
       MDS[m.idx , i] <- NA
     }
+      df_wide <- rbind( data, MDS) %>% mutate( ds = c(rep("df", NROW(data)), rep("df_miss", NROW(MDS))  ))
+      
+      long<- melt(df_wide, id.vars = "ds")
+      
+      densplot <-ggplot(long, aes(x=value, fill = ds)) + 
+              geom_density(alpha=.4) + 
+              facet_wrap(~variable, ncol=2, scales = "free_x")
+      
+      misspercplot <- gg_miss_var(MDS, show_pct = T)
+      
   } else {
     for(i in 1:length(column)){
       m.idx <- (rbinom(n=NROW(data), size=1, prob=q*(U > p))==1)
-      MDS[m.idx ,column[i]] <- NA}}
+      MDS[m.idx ,column[i]] <- NA}
+      data_par<-data[, column]
+      MDS_par<-MDS[, column]
+      
+      df_wide <- rbind( data_par, MDS_par) %>% mutate( ds = c(rep("df", NROW(data)), rep("df_miss", NROW(MDS))  ))
+      
+      long<- melt(df_wide, id.vars = "ds")
+      
+      densplot <-ggplot(long, aes(x=value, fill = ds)) + 
+        geom_density(alpha=.4) + 
+        facet_wrap(~variable, ncol=2, scales = "free_x")
+      
+      misspercplot <- gg_miss_var(MDS, show_pct = T)
+        
+    }
   
-  return(MDS)
+      output <- list(MDS, densplot, misspercplot)
+  return(output)
 }
--- R/package_trial1.R
+++ R/package_trial1.R
@@ -11,10 +11,9 @@ mcar <- function(data, p, column = NULL){
     for(i in 1:length(column)){
       m.idx <- sample(x=1:NROW(data), size= p*NROW(data), replace=FALSE)
       MDS[m.idx ,column[i]] <- NA}}
-  bmiss<-ggplot(data, aes(x=norm))+ geom_histogram()
-  amiss<-ggplot(MDS, aes(x=norm))+ geom_histogram()
+
   percent_miss<-gg_miss_var(MDS, show_pct = T)
   
-  output<- list(MDS, bmiss, amiss, percent_miss)
+  output<- list(MDS, percent_miss)
   return(output)
 }
