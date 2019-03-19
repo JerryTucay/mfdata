2e759ca10cc9d9f220f4957d68685f991938685b
mathistite <tucayjerry3@gmail.com>
2019-03-18 19:32:12 -0700

    put graphs in for mar

--- R/nmar.R
+++ R/nmar.R
@@ -1,11 +1,9 @@
-
-
 nmar <- function(data, p, q, column = NULL){
   if(is.null(column)){
     MDS <- data.frame(apply(data, 2, function(x){
       cutoff <- max(x)*p
       x.na <- x
-      idx  <- which(x>cutoff)
+      idx  <- which(x > cutoff)
       x.na[idx] <- ifelse(rbinom(n=length(idx), size=1, prob=q)==1, NA, x.na[idx])
       return(x.na)
     }))
@@ -40,6 +38,6 @@ nmar <- function(data, p, q, column = NULL){
     
     misspercplot <- gg_miss_var(MDS, show_pct = T)
   }
-  output<- c(MDS, densplot, misspercplot)
+  output<- list(MDS, densplot, misspercplot)
   return(output)
 }
