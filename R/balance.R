0a599ab436b219c7e02f576d04d97a1b98cd26f9
Jerry Tucay <tucayjerry3@gmail.com>
2019-04-25 11:04:58 -0700

    added graphs to balacne

--- R/balance.R
+++ R/balance.R
@@ -12,13 +12,21 @@ balance <- function(data, pi, choice){
   for(i in 1:rows){
     any_error[i, ] <- matrix(rbinom(3,1,pi[i,]), nrow=1)
   }
-
+  
   error_frame <- any_error*size
   X <- data
   for(i in length(choice)){
   X[,choice[i]] <- data[,choice[i]] + error_frame}
   
-  output <- list(X, any_error)
+  df_wide <- rbind( data, X) %>% mutate( ds = c(rep("df", NROW(data)), rep("df_error", NROW(X))  ))
+  
+  long<- melt(df_wide, id.vars = "ds")
+  
+  densplot <-ggplot(long, aes(x=value, fill = ds)) + 
+    geom_density(alpha=.4) + 
+    facet_wrap(~variable, ncol=2, scales = "free_x")
+  
+  output <- list(X, any_error, densplot)
   return(output)
 }
 
